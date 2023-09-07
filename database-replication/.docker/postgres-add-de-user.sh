e0b41e1c0dd5285ce866a70bad76230af1a5147b
lelouvincx <dinhminhchinh3357@gmail.com>
2023-09-07 07:15:57 +0700

    Fix source_db's restoring process

--- database-replication/.docker/postgres-add-de-user.sh
+++ database-replication/.docker/postgres-add-de-user.sh
@@ -2,6 +2,13 @@
 set -e
 
 PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER} --dbname ${POSTGRES_DB} <<-EOSQL
+	  CREATE USER azure_pg_admin;
+	  GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO azure_pg_admin;
+	  CREATE USER azure_superuser;
+	  ALTER USER azure_superuser WITH SUPERUSER;
+	  GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO azure_superuser;
+	  CREATE USER greglow;
+	  GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO greglow;
 	  CREATE USER data_engineer;
 	  GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO data_engineer;
 EOSQL
